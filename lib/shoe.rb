166c680572b8447f091ffeb5f37e35c64f0686bb
clei1 <callmecon328@gmail.com>
2018-07-21 17:15:59 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,10 +2,10 @@
 
 class Shoe
   attr_accessor :author, :page_count, :genre
-  attr_reader :title
+  attr_reader :brand
 
-  def initialize(title)
-    @title = title
+  def initialize(brand)
+    @brand = brand
   end
 
   def turn_page
